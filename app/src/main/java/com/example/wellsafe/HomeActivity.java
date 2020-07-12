e964201a8fc20cf659707549863945b73975cb4a
jonathanlee06 <43515846+jonathanlee06@users.noreply.github.com>
2020-07-12 20:40:14 +0800

    Minor update on bottom navigation

--- app/src/main/java/com/example/wellsafe/HomeActivity.java
+++ app/src/main/java/com/example/wellsafe/HomeActivity.java
@@ -32,6 +32,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -60,12 +64,12 @@ public class HomeActivity extends AppCompatActivity {
         //new HomeFragment();
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        /* AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_stats, R.id.navigation_scan, R.id.navigation_profile)
+        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_stats, R.id.nav_checkin, R.id.nav_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController); */
+        NavigationUI.setupWithNavController(navView, navController);*/
     }
 
 
